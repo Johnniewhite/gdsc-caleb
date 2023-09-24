58c6e4a4d4d36d29563f7792590186593b5c6a33
Adeyinka Inioluwa John <boluwatifeeri@gmail.com>
2023-09-24 14:51:43 +0100

    FEAT: Completed the navigation menu

--- src/app/HomePage.jsx
+++ src/app/HomePage.jsx
@@ -1,10 +1,10 @@
-import Header from './components/Header'
+
 import React from 'react'
 
 export default function HomePage() {
   return (
     <div>
-      <Header />
+      
       
     </div>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import Header from "./components/Header";
 import HomePage from "./HomePage";
+
 export default function Home() {
   return (
     <>
+      <Header />
       <HomePage />
     </>
   );
